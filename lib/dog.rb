b7ac190820bb34b0f48f1787aacaa7382a862d74
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-10 11:25:40 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Dog
+  attr_accessor :name
+  @@all = Array.new
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all = Array.new
+  end
+
+  def self.all
+    @@all.each {|dog| puts dog.name}
+  end
+
+end
